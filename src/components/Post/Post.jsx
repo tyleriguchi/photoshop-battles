27a2bf660e25c2bd3efcdcf9c50c5ddedaeaa2e5
tyleriguchi <tyleriguchi@gmail.com>
2016-07-03 15:34:55 -0700

    message if no comments

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -13,18 +13,35 @@ class Post extends React.Component {
 
     fetchPostComments(post);
   }
+  renderComments() {
+    const { comments } = this.props;
 
+    if (comments.length > 0) {
+      return (
+        <div>
+          {comments.map( (comment, idx) => (
+            <CommentItem
+              key={idx}
+              comment={comment} />
+          ))}
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          {'No shops found :('}
+        </div>
+      )
+    }
+  }
   render() {
     const { comments } = this.props;
-    
+
     return (
       <div>
         {this.props.post['title']}
-        {comments.map( (comment, idx) => (
-          <CommentItem
-            key={idx}
-            comment={comment} />
-        ))}
+        {this.renderComments()}
       </div>
     )
   }
