9c890e7782862d4f5eef9ff0a066af1ecc6edcf8
tyleriguchi <tyleriguchi@gmail.com>
2016-07-03 15:35:11 -0700

    fetch on mount

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -2,27 +2,24 @@ import React from 'react'
 // import classes from './Counter.scss'
 import ListItem from '../PostListItem';
 
-export const RedditPostList = (props) => {
-  const posts = props.posts || [];
+export default class RedditPostList extends React.Component {
+  componentDidMount() {
+    const { fetchPosts } = this.props;
 
-  return (
-    <div>
-      <button onClick={props.fetchPosts}>Fetch</button>
-      {posts.map( (post, idx) => (
+    fetchPosts();
+  }
+
+  render() {
+    const { posts } = this.props;
+
+    return (
+      <div>
+        {posts.map( (post, idx) => (
           <ListItem
             key={idx}
             post={post} />
-        )
-
-    )}
-    </div>
-  )
+        ))}
+      </div>
+    )
+  }
 }
-
-RedditPostList.propTypes = {
-  // counter: React.PropTypes.number.isRequired,
-  // doubleAsync: React.PropTypes.func.isRequired,
-  // increment: React.PropTypes.func.isRequired
-}
-
-export default RedditPostList
