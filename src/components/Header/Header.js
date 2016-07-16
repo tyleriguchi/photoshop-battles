010c46590303f9f8fbe1abeed3e19241ecc51f96
tyleriguchi <tyleriguchi@gmail.com>
2016-07-16 13:17:01 -0700

    rename link header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import classes from './Header.scss'
 export const Header = () => (
   <div>
     <Link to='/list' activeClassName={classes.activeRoute}>
-      List
+      Shops
     </Link>
   </div>
 )
